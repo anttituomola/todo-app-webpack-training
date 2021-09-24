411af9e11971c0b190069f5a9ae6e60f7d2b6057
Antti <anttituomola8@gmail.com>
2021-09-24 21:05:23 +0300

    initial commit

--- src/app/app.js
+++ src/app/app.js
@@ -1,2 +1,7 @@
-const test = 'Success'
-document.getElementById('test').innerHTML = test;
+import { addTodo } from "./addTodo"
+
+let inputEl = document.getElementById('inputEl');
+let submitButton = document.getElementById('submitButtonEl');
+let renderEl = document.getElementById('renderEl');
+
+submitButton.addEventListener("click", addTodo);
---
+++ src/todo.js
@@ -0,0 +1,8 @@
+export class todo {
+    constructor(text, done, dueDate, id) {
+        this.text = text;
+        this.done = done;
+        this.dueDate = dueDate;
+        this.id = id;
+    }
+}
