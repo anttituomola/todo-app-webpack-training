8dadfdc801e57470b8dd015746fd7cf3988f8896
Antti <anttituomola8@gmail.com>
2021-09-27 20:29:54 +0300

    check if input is empty

--- src/app/app.js
+++ src/app/app.js
@@ -4,4 +4,11 @@ export let todos = []
 
 let submitButton = document.getElementById('submitButtonEl');
 
-submitButton.addEventListener("click", addTodo);
+submitButton.addEventListener("click", () => {
+    if (document.getElementById("inputEl").value != "") {
+        addTodo()
+    }
+    else {
+        document.getElementById("inputEl").classList.toggle("missingInput")
+    }
+})
