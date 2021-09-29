156561ab23e3720a013006ad744c6b627adfc4e2
Antti <anttituomola8@gmail.com>
2021-09-29 20:59:37 +0300

    adding trash icons

--- src/app/render.js
+++ src/app/render.js
@@ -5,7 +5,14 @@ var dayjs = require('dayjs')
 export function render() {
     renderEl.innerHTML = ""
         for (let todoData of todos) {
-            renderEl.innerHTML += `<div class="todoElement"><span class="todoText">${todoData.text}</span> <span class="todoDueDate">${dayjs(todoData.dueDate).format("DD.MM.YYYY")}</span></div>`
+            renderEl.innerHTML += `
+            <div class="todoRow">
+                <div class="todoElement">
+                    <span class="todoText">${todoData.text}</span>
+                    <span class="todoDueDate">${dayjs(todoData.dueDate).format("DD.MM.YYYY")}</span>
+                </div>
+                <i class="trashicon far fa-trash-alt" />
+            </div>`
         }
     document.getElementById("inputEl").focus()
 }
