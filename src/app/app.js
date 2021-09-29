23e50a466bd0a26465e6b787ad56a4076597cc7f
Antti <anttituomola8@gmail.com>
2021-09-29 21:54:57 +0300

    deleteHandler

--- src/app/app.js
+++ src/app/app.js
@@ -4,10 +4,13 @@ export let todos = []
 
 let submitButton = document.getElementById('submitButtonEl');
 
+//Listen for button click
 submitButton.addEventListener("click", () => {addTodo()})
 
+//Listen for Enter
 document.getElementById("inputEl").addEventListener("keypress", (e) => {
     if(e.key === "Enter") {addTodo()}
 })
 
+//Initial focus on input
 document.getElementById("inputEl").focus()
--- src/app/render.js
+++ src/app/render.js
@@ -1,18 +1,27 @@
 import { todos } from "./app"
+import {deleteHandler} from "./deleteHandler"
+
 let renderEl = document.getElementById('renderEl');
 var dayjs = require('dayjs')
 
 export function render() {
     renderEl.innerHTML = ""
-        for (let todoData of todos) {
-            renderEl.innerHTML += `
+    for (let todoData of todos) {
+        renderEl.innerHTML += `
             <div class="todoRow">
                 <div class="todoElement">
                     <span class="todoText">${todoData.text}</span>
                     <span class="todoDueDate">${dayjs(todoData.dueDate).format("DD.MM.YYYY")}</span>
                 </div>
-                <i class="trashicon far fa-trash-alt" />
+                <i id="${todoData.id}" class="trashicon far fa-trash-alt" />
             </div>`
-        }
+    }
     document.getElementById("inputEl").focus()
+
+    let icons = document.querySelectorAll(".trashicon")
+    icons.forEach(function(elem) {
+        elem.addEventListener("click", function(e) {
+            deleteHandler(e)
+        })
+    })
 }
